dc966bc985861fcd192222073d4eeae9e4c9e811
Jens Cornelis <mail@jenscornelis.de>
2012-05-27 21:11:17 +0200

    Naive collision detection and handling

--- verletPygame.py
+++ verletPygame.py
@@ -5,7 +5,7 @@ Mass Spring System. This has just been written to test pygame.
 Require: numpy, pygame
 
 TODO: 
-- add collision handling and response between objects
+- add less naive collision handling and response between objects
 
 Author: Jens Cornelis
 """
@@ -15,22 +15,31 @@ import math
 import pygame
 import sys
 
-#globals
-world_size = 1000,1000  
+#globalsb
+world_size = 1000,700  
 world_rect = pygame.Rect(0,0, world_size[0], world_size[1])
 num_iterations = 10 #iteration for constraint relaxation
 pickedParticle = None
 mousePosition = 0,0 
+frames = 25
+drawVelocities = False
+
+class Material:
+	def __init__(self, stiffness=0.3, friction=0.1):
+		self.stiffness = stiffness
+		self.friction = friction
 
 class MSSObject:
-	def __init__(self, vertices, indexedSprings, screen):
+	def __init__(self, vertices, indexedSprings, screen, material):
 		self.screen = screen
 		self.particles = []
-		self.material = Material()
+		self.material = material
 
 		#set up particles
 		for vertex in vertices:
 			self.particles.append(Particle(vertex, screen, self))
+
+		self.mass = len(self.particles)
 
 		#set up springs/constraints
 		self.constraints = []
@@ -39,6 +48,26 @@ class MSSObject:
 
 		#initial draw
 		self.draw()
+
+	def setMass(self, mass):
+		self.mass = mass
+		partialMass = mass / float(len(self.particles))
+		for particle in self.particles:
+			particle.mass = partialMass
+
+	def get_rect(self):
+		x_min, y_min = world_size[0], world_size[1]
+		x_max, y_max = 0, 0
+		for particle in self.particles:
+			if particle.x[0] > x_max:
+				x_max = particle.x[0]
+			if particle.x[0] < x_min:
+				x_min = particle.x[0]
+			if particle.x[1] > y_max:
+				y_max = particle.x[1]
+			if particle.x[1] < y_min:
+				y_min = particle.x[1]
+		return pygame.Rect(x_min, y_min, x_max - x_min, y_max - y_min)
 
 	def update(self):
 		for particle in self.particles:
@@ -53,10 +82,35 @@ class MSSObject:
 		for particle in self.particles:
 			particle.draw()
 
-class Material:
-	def __init__(self, stiffness=0.1):
-		self.stiffness = stiffness
-		self.friction = 0.1
+class DeformableSphere(MSSObject):
+	def __init__(self, center, radius, vertices, indexedSprings, screen, material=Material()):
+		MSSObject.__init__(self, vertices, indexedSprings, screen, material)
+		self.radius = radius
+		self.center = center
+
+	def movePointOut(self, point):
+		direction = point - self.center
+		directionLength = linalg.norm(direction)
+		normalizedDirection = direction/directionLength
+		distMax = 0
+		for particle in self.particles:
+			tmp = dot(normalizedDirection, particle.x - self.center)
+			if tmp > distMax:
+				distMax = tmp
+		if linalg.norm(direction) < distMax:
+			diff = (self.radius - directionLength)/directionLength	
+			return direction*diff
+		else:
+			return (0,0)
+
+
+class DeformableCube(MSSObject):
+	def __init__(self, vertices, indexedSprings, screen, material=Material()):
+		MSSObject.__init__(self, vertices, indexedSprings, screen, material)
+		raise Error("Not yet implemented")
+
+	def movePointOut(self, point):
+		return (0,0)
 
 class Constraint:
 	def __init__(self, p1, p2):
@@ -74,6 +128,7 @@ class Particle:
 		self.image = pygame.image.load("sphere.png")
 		self.picked = False
 		self.parentObject = parentObject
+		self.velocity = array([0,0]) 
 
 		#set bounding volume and position
 		self.bv = self.image.get_rect()
@@ -87,6 +142,8 @@ class Particle:
 
 	def draw(self):
 		self.screen.blit(self.image, self.bv)
+		if drawVelocities:
+			pygame.draw.line(self.screen, (255,0,0), self.x, self.x + self.velocity)
 
 	def update(self):
 		self.bv[0] = self.x[0] - self.radius
@@ -97,7 +154,7 @@ def computeForces(objects):
 	#add gravitational forces and friction
 	for obj in objects:
 		for particle in obj.particles:
-			particle.force = array([0.0, particle.mass * 9.81])
+			particle.force = array([0.0, particle.mass * 9.81 * 100.0])
 
 def computeFriction(objects):
 	for obj in objects:
@@ -129,7 +186,10 @@ def verlet(h, objects):
 			oldx = particle.oldx
 			a = particle.force / particle.mass
 			particle.x += x - oldx + a*h*h
+			#compute velocity as central difference of positions
+			particle.velocity = (particle.x - particle.oldx)/(2.0*h)
 			particle.oldx = tmp
+	
 
 def satisfyConstraints(objects):
 	for val in range(num_iterations):
@@ -161,12 +221,20 @@ def satisfyConstraints(objects):
 					diff = (0 - deltalength)/deltalength
 					pickedParticle.x += delta*diff
 
-			#TODO: Collision detection and handling
-
-def create2DBall(screen, center, radius, particles):
+			for particle in obj.particles:
+				for obj2 in objects:
+					if obj == obj2:
+						continue
+					else:
+						if obj2.get_rect().collidepoint(particle.x):
+							particle.x += obj2.movePointOut(particle.x)
+							
+def create2DBall(screen, center, radius, particles, material):
+	'''Convenience method to generate a ball mesh'''
 	p = []
 	p.append(center)
-	for angle in range(0, 360, int(360/particles)):
+	for val in range(particles):
+		angle = val * 360./particles
 		tmp_x = center[0] + radius*math.cos((angle*math.pi)/180.)
 		tmp_y = center[1] + radius*math.sin((angle*math.pi)/180.)
 		p.append(array([tmp_x, tmp_y]))
@@ -178,7 +246,17 @@ def create2DBall(screen, center, radius, particles):
 	c.append((1, len(p) - 1))
 	c.append((0, len(p) - 1))
 	
-	return MSSObject(p, c, screen)
+	return DeformableSphere(center, radius, p, c, screen, material)
+
+def create2DCube(screen, rect, material):
+	p = []
+	p.append(array([rect[0], rect[1]]))
+	p.append(array([rect[0], rect[1] + rect[3]]))
+	p.append(array([rect[0] + rect[2], rect[1]]))
+	p.append(array([rect[0] + rect[2], rect[1] + rect[3]]))
+	c = (0,1), (1,2), (2,3), (3, 0), (0,2), (1,3)
+
+	return DeformableCube(p, c, screen, material)
 
 
 def main():
@@ -189,15 +267,19 @@ def main():
 	
 	#setting up objects
 	objects = []
-	
-	objects.append(create2DBall(screen, array([550., 550.]), 100., 8))
+	mat1 = Material(0.9)
+	mat2 = Material(0.6)
+	sphere = create2DBall(screen, array([300., 300.]), 100., 8, mat2)
+	objects.append(sphere)
+	sphere2 = create2DBall(screen, array([100., 100.]), 80., 7, mat1)
+	objects.append(sphere2)
 
 	clock = pygame.time.Clock()
 
 	#main simulation loop
 	while True:
-		#set clock of pygame to 50 for equal timesteps
-		clock.tick(50)
+		#set clock of pygame to predefined frames for equal timesteps
+		clock.tick(frames)
 
 		#clear screen with background color
 		screen.fill((80,80,80))
@@ -225,7 +307,7 @@ def main():
 		computeFriction(objects)
 
 		#compute timestep according to frame set py pygame.clock
-		h=50./1000.
+		h=frames/1000.
 
 		#do integration step and satisfy constraints
 		verlet(h, objects)
